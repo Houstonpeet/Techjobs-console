73eb246ff04d49a1ed8499be9c3fc7f8ee271942
Houston <Peethouston@gmail.com>
2023-02-16 20:10:55 -0600

    assignment 1

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -2,6 +2,7 @@ import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
